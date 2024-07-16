dff58ada574c27df9ff8d211af86b068fcad368d
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-16 18:30:09 +0200

    Create Contact.py

---
+++ Contact.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def main():
+    st.title("Elérhetőség")
+
+    st.write("""
+    ### Kapcsolat
+    - **Email:** aba.lorincz@gmail.com
+    - **Telefon:** +36 30 954 2176
+    - **Cím:** Department of Thermophysiology, Institute for Translational Medicine, Medical School, University of Pécs, 12 Szigeti Street, 7624 Pécs, Hungary
+    """)
+
+if __name__ == "__main__":
+    main()
