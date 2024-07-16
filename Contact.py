2cb38ae7b23d2ab3236d3f3c07aa662ca78f009b
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-16 18:38:31 +0200

    Update Contact.py

--- Contact.py
+++ Contact.py
@@ -8,7 +8,7 @@ def main():
     - **Email:** aba.lorincz@gmail.com
     - **Telefon:** +36 30 954 2176
     - **Cím:** Department of Thermophysiology, Institute for Translational Medicine, Medical School, University of Pécs, 12 Szigeti Street, 7624 Pécs, Hungary
-    - **Forráskód:**https://github.com/Weston0793/x-ray-database/
+    - **Forráskód:** https://github.com/Weston0793/x-ray-database/
     """)
 
 if __name__ == "__main__":
