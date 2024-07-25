11b7dfe92c3a467fe6bbe505957127c2a7347345
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-25 04:36:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 from multipage import MultiPage
-from Home import main as home
+from Upload import main as upload
 from Search import search_section as search
 from Status import main as status
 from Contact import main as contact
+from Home import main as home
 
 app = MultiPage()
 
 # Add all your applications (pages) here
-app.add_page("Kép feltöltése", home)
+app.add_page("Főoldal", home)
+app.add_page("Kép feltöltése", upload)
 app.add_page("Képek keresése", search)
 app.add_page("Státusz", status)
 app.add_page("Elérhetőség", contact)
