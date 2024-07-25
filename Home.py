561bbaea977758b8a931cd56ef383490cb28d1ef
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-25 04:48:19 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,7 +17,7 @@ def main():
 
     #### 1. Kép feltöltése
     - Kérjük a feltöltésre szánt képekről bizonyosodjon meg hogy anonimizálva vannak! A képeken nem szerepelhet semmilyen betegazonosító!
-    - Válassza a bal oldalsáv "Navigáció" menüjéből a "Kép feltöltése" menüpontot .
+    - Válassza a bal oldalsáv "Navigáció" menüjéből a "Kép feltöltése" pontot.
     - Húzza a "Drag and drop file here" mezőbe a képet vagy válassza ki a "Browse files" gombbal s töltse fel a röntgenkép(ek)et (Max 15 MB/file).
     - **Több kép feltöltése**: Ha egyszerre több képet szeretne feltölteni, pipálja ki a "Több kép feltöltése" lehetőséget. Figyelem: az összes kép ugyanazokat a címkéket kapja! (kivéve a betegazonosítót)
     - Kérem, adja meg a kötelező adatokat: korcsoport, röntgen nézet, normál vagy elváltozás típusa, melyik oldal (ha végtagról van szó), és a sérült régiók (fő régió, régió).
@@ -27,13 +27,13 @@ def main():
     - Kattintson a "Feltöltés" gombra a kíválasztott adatok újra összegzéséhez, majd a "Megerősítés és Feltöltés" gombbal véglegesítheti a feltöltést.
 
     #### 2. Képek keresése
-    - Válassza a "Képek keresése" menüpontot.
+    - Válassza a "Képek keresése" menüpontot a bal oldalsáv "Navigáció" menüjéből.
     - Adja meg a keresési feltételeket (minimum: típus, nézet, főrégió).
     - Kattintson a "Keresés" gombra. A találatok listája megtekinthető és letölthető. 
     - Várjon egy pár másodpercet amíg a szerver összeállítja a "Letöltés" gomb megnyomása után a .zip filet, majd kattintson a "Megerősítés s Letöltés" gombra ha le kívánja tölteni a képeket és a hozzájuk tartozó címkéket. 
 
     #### 3. Státusz
-    - Válassza a "Státusz" menüpontot.
+    - Válassza a "Státusz" menüpontot a bal oldalsáv "Navigáció" menüjéből.
     - Tekintse meg a feltöltött röntgenképek statisztikáit és a projekt aktuális fázisának állapotát.
     - Az adatok alapján nyomon követheti a projekt előrehaladását és a hiányzó elemeket.
     - Az első fázis lezárási kirétirumai: az összes különböző csontot tartalmazó régióból, legalább két nézetből, normál és törött röntgenképeket gyüjts felnőttektől, kombinációnként legalább 50 darabot.
@@ -41,7 +41,7 @@ def main():
     - Fontos: egyelőre a státusz a gyermekkori röntgeneket is számba veszi!
 
     #### 4. Elérhetőség
-    - Válassza az "Elérhetőség" menüpontot.
+    - Válassza az "Elérhetőség" menüpontot a bal oldalsáv "Navigáció" menüjéből.
     - Ha bármilyen észrevétele van a honlappal kapcsolatban, segítségre van szüksége vagy kérdése van, lépjen nyugodtan kapcsolatba a fejlesztőkkel.
 
     ### Fontos Információk
